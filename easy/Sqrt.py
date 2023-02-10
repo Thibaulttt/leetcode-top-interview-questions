322d0d7a3ad71704fb3b66e8e4ce219119cdbf0d
Thibaulttt <thibault.le.guillou79@gmail.com>
2023-02-10 09:52:20 +0100

    Resolve 69. Sqrt(x)

---
+++ easy/Sqrt.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(x**0.5)
+
+solution = Solution()
+print(solution.mySqrt(4))
+print(solution.mySqrt(13))
+print(solution.mySqrt(1))
