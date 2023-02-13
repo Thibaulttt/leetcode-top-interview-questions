aae06063a8b7a647610288211334780e4251c133
Thibaulttt <thibault.le.guillou79@gmail.com>
2023-02-13 10:12:00 +0100

    Resolve 387. First Unique Character in a String

--- easy/First Unique Character in a String.py
+++ easy/First Unique Character in a String.py
@@ -1,20 +1,30 @@
 class Solution:
+    # def firstUniqChar(self, s: str) -> int:
+    #     uniq = False
+    #     for i in range(len(s)-1):
+    #         for c in s[i+1:]:
+    #             uniq = True
+    #             if s[i] == c:
+    #                 uniq = False
+    #                 break
+    #         print(uniq, s[i])
+    #         if uniq:
+    #           return i
+    #     return -1
+    
     def firstUniqChar(self, s: str) -> int:
-        uniq = False
-        for i in range(len(s)-1):
-            for c in s[i+1:]:
-                uniq = True
-                if s[i] == c:
+        for i in range(len(s)):
+            uniq = True
+            for j in range(len(s)):
+                if i!=j and s[i] == s[j]:
                     uniq = False
                     break
-            print(uniq, s[i])
             if uniq:
-              return i
-            
+                return i
         return -1
 
 solution = Solution()
-# s = "leetcode"
-s = "loveleetcode"
-s = "aabb"
+s = "leetcode"
+# s = "loveleetcode"
+# s = "aabb"
 print(solution.firstUniqChar(s))
