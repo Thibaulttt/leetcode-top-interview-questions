f206df5d86d948d5b1479b2ea14920c67c9b3978
Thibaulttt <thibault.le.guillou79@gmail.com>
2023-03-03 14:25:27 +0100

    Resolve 118. Pascal's Triangle

--- easy/Pascal's Triangle.py
+++ easy/Pascal's Triangle.py
@@ -7,52 +7,24 @@ from typing import List
 # 1 3 3 1
 # 1 4 6 4 1
 
-# class Solution:
-#   def generate(self, numRows: int) -> List[List[int]]:
-#     res = [[1]]
-
-#     for i in range(1, numRows):
-#       row = []
-#       for j in range(0,i+1):
-#         print(i-1, j-1, i, j)
-#         row.append(res[i-1][j-1])
-#         if j != i:
-#           print(res[i-1][j])
-#           row.append(res[i-1][j-1] + res[i-1][j])
-#       res.append(row)
-#       print(res)
-
-#     return res
-
-# solution = Solution()
-
-# numRows = 4
-
-# print(solution.generate(numRows))
-
 class Solution:
   def generate(self, numRows: int) -> List[List[int]]:
-    res = []
+    if numRows == 1:
+      return [1]
+    
+    elif numRows == 2:
+      return [1,1]
+    
+    res = [[1], [1, 1]]
 
-    res.append([1])
-    res.append([1, 1])
-
-    for i in range(2, numRows):
-      row = []
-
-      row.append(1)
-      for j in range(1, i-1):
-        print(i, j, int((i+1)/2), res)
-        if j < int((i+1)/2):
-          row.append(res[i-1][j-1] + res[i-1][j])
-        if j > int((i+1)/2):
-          row.append(res[i-1][j-2] + res[i+1][j-1])
-        # elif j != i:
-        #   row.append(res[i-1][j-1] + res[i-1][j-1])
-        # else:
-        #   row.append(res[i-1][j-1])
-      row.append(1)
-      res.append(row)
+    for i in range(2,numRows):
+      res.append([])
+      for j in range(len(res[i-1])+1):
+        if j == 0 or j == len(res[i-1]):
+          res[i].append(1)
+        else:
+          previous_sum = res[i-1][j-1] + res[i-1][j]
+          res[i].append(previous_sum)
 
     return res
 
